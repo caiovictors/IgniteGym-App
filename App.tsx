24fa6d36a3ecf7e69c784e8ea21145893688f95d
caiovictors <caiosarmento@eng.ci.ufpb.br>
2023-01-28 14:11:18 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base'
+import { THEME } from './src/theme'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Loading } from '@components/Loading';
+import { SignIn } from '@screens/SignIn';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
